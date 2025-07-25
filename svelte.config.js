85e9d94d20f8cb77229b1b53b535b00179865172
ricomonster <2063313+ricomonster@users.noreply.github.com>
2025-07-25 15:20:42 +0800

    feat: updated svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,26 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import path from 'node:path'
+import path from 'node:path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: {
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  kit: {
     adapter: adapter(),
     files: {
+      assets: 'static',
       routes: 'src/app/routes/',
-      lib: 'src',
       appTemplate: 'src/app/app.html',
-      assets: 'public',
       hooks: {
         server: 'src/app/hooks.server.ts',
       }
     },
     alias: {
-      $routes: '.svelte-kit/types/src/app/routes',
       $app: path.resolve('./src/app'),
       $lib: path.resolve('./src/lib'),
+      $routes: '.svelte-kit/types/src/app/routes',
       $src: path.resolve('./src'),
     }
   }
